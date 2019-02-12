a423e39407e04d01e08d35a66d0a4ea9419011da
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-12 05:34:41 +0000

    Implemented support for command line arguments via "process.argv"

--- download_avatars.js
+++ download_avatars.js
@@ -1,24 +1,48 @@
 var request = require("request");
 var fs = require("fs");
+var secret = require("./secret.js");
+var repoName = process.argv[2];
+var repoOwner = process.argv[3];
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "request",
+      Authorization: "token e7b2365d90b35893632f79c93a915433b8345600"
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+function downloadImageByURL(url, filePath) {
+  var path = "./avatars/";
+
+  request
+    .get(url)
+    .on("error", function(err) {
+      throw err;
+    })
+    .on("response", function(response) {
+      console.log("Response Status Code: ", response.statusCode);
+    })
+    .pipe(fs.createWriteStream(path + filePath));
+}
+
+if (process.argv[2] === undefined || process.argv[2] === undefined) {
+  console.log("ERROR: please enter an argument");
+}
+
+getRepoContributors("repoOwner", "repoName", function(err, result) {
+  var list = JSON.parse(result);
+  for (var i = 0; i < list.length; i++) {
+    avatar_url = list[i].avatar_url;
+    user_name = list[i].login + ".jpg";
+    downloadImageByURL(avatar_url, user_name);
+  }
 });
-
-// var requestOptions = {
-//   url: "https://api.github.com/repos/jquery/jquery/contributors",
-//   json: true
-// };
-
-// function list(cont) {
-//   console.log(cont);
-// }
-
-// list(requestOptions, list);
