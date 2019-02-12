5bcb26f92ff5291e70577b9ebf0eca08bfaa8d6e
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-12 03:50:50 +0000

    Created callback function to handles errors

--- download_avatars.js
+++ download_avatars.js
@@ -3,6 +3,15 @@ var fs = require("fs");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
 // var requestOptions = {
 //   url: "https://api.github.com/repos/jquery/jquery/contributors",
 //   json: true
